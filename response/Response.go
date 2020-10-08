88c0b92ad8f856366b07fa7db09381d0789c6dcc
Muhammad Suryono <msuryono0@gmail.com>
2020-10-08 22:35:44 +0700

    Add response

---
+++ response/Response.go
@@ -0,0 +1,15 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+func Json(code int, message string, obj interface{}) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(code, gin.H{"code": code, "message": message, "data": obj})
+	}
+}
+
+func App(app string, message string, author string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"app_name": app, "version": message, "author": author})
+	}
+}
