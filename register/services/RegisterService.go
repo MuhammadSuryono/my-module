2ec627c367b6878d557b251066d6e2ad7f26ca1c
Muhammad Suryono <msuryono0@gmail.com>
2020-10-11 10:15:11 +0700

    Update response

--- register/services/RegisterService.go
+++ register/services/RegisterService.go
@@ -41,5 +41,5 @@ func RegisterUser(credential *models.TMerchant) bool {
 }
 
 func (info *registerInformation) RegisterStatic(devid string, nohp string) bool {
-	return info.device_id == devid && info.no_hp == nohp
+	return "123456789" == devid && "0895355698652" == nohp
 }
