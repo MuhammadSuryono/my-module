fdd8c2a530313de7489478347622c0aebaeb377f
Muhammad Suryono <msuryono0@gmail.com>
2020-10-11 09:36:24 +0700

    Update rsa

--- response/Response.go
+++ response/Response.go
@@ -2,14 +2,12 @@ package response
 
 import "github.com/gin-gonic/gin"
 
-func Json(code int, message string, obj interface{}) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSON(code, gin.H{"code": code, "message": message, "data": obj})
-	}
+func Json(code int, message string, obj interface{}) {
+	var c gin.Context
+	c.JSON(code, gin.H{"code": code, "message": message, "data": obj})
 }
 
 func App(app string, message string, author string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"app_name": app, "version": message, "author": author})
-	}
+	var c gin.Context
+	c.JSON(200, gin.H{"app_name": app, "version": message, "author": author})
 }
